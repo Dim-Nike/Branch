95b82bf17358336f23d7833b06cc85ac1031d3f5
Dim-Nike <Dim_Nike@mail.ru>
2020-11-04 13:47:18 +0300

    All upgrade

--- Code-master.cpp
+++ Code-master.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -53,6 +54,48 @@ public:
 
 };
 
+class ymn :public fin
+{
+	private:
+	int x;
+	int y;
+
+	public:
+	ymn(int x, int y)
+	{
+	this->x=x;
+	this->y=y;
+	}
+
+	void option() override
+	{
+	int c = x * y;
+	cout<<x<<" * "<<y<<" = "<<c<<endl;
+	}
+};
+
+class povv :public fin
+{
+	private:
+	int x;
+	int y;
+
+	public:
+	povv(int x, int y)
+	{
+	this->x=x;
+	this->y=y;
+	}
+
+	void option() override
+	{
+		int c=pow(x,y);
+		cout<<x<<" в степени "<<y<<" = "<<c<<endl;
+	}
+
+};
+
+
 class Print :public fin
 {
 public:
@@ -64,13 +107,41 @@ public:
 
 int main()
 {
-	sum sum(7,5);
-	interest interest(79,34);
-	dif dif(8,5);
-	Print v1;
-	v1.display(&sum);
-	v1.display(&interest);
-	v1.display(&dif);
+	
+	int a;
+	int x,y;
+
+	cout<<"Введите два числа"<<endl;
+	cin>>x>>y;
+	sum sum(x,y);
+	dif dif(x,y);
+	ymn ymn(x,y);
+	povv povv(x,y);
+	Print var;
+	cout<<"Главное меню:\n1.Сложение\n2.Вычитание\n3.Умножение\n4.Возведение в степень"<<endl;
+	cin>>a;
+	switch (a)
+	{
+		case 1:
+		var.display(&sum);
+		break;
+		
+		case 2:
+		var.display(&dif);
+		break;
+
+		case 3:
+		var.display(&ymn);
+
+		case 4:
+		var.display(&povv);
+		break;
+
+
+		default:
+		cout<<"Ошибка,вы вышли из поограммы"<<endl;
+		
+	}
 
 	return 0;
 }
